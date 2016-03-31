defb85d7fdbc4c08cf325c2cac517c12ea266a2d
Emma Forsling <emma.forsling.parborg@gmail.com>
2016-03-31 14:25:18 +0200

    fixed untracked files

--- build/CMakeFiles/3.2.1/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/i386/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/martingrad/Documents/LiTH/TNCG14/Depth-of-Field-Project/build/CMakeFiles/3.2.1/CompilerIdC/CMakeCCompilerId.c
--- build/Tutorials.build/Debug/ZERO_CHECK.build/Script-9E2C68F69CF34D6A8DCD10E8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/martingrad/Documents/LiTH/TNCG14/Depth-of-Field-Project/build -f /Users/martingrad/Documents/LiTH/TNCG14/Depth-of-Field-Project/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
